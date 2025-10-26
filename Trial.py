7fbef6cecd7b8a4a37b99eaeda0150f26b1e5bbf
Purusottam Rooj <purusottamrooj2021@gmail.com>
2025-10-27 00:35:05 +0530

    trial

--- Trial.py
+++ Trial.py
@@ -20,3 +20,65 @@ for i in range(N):
 
 pi=4*count/N
 print(pi)
+
+
+def f(x):
+    return x**2
+
+N=1000000
+s=0
+for i in range(N):
+    x=random.uniform(0,5)
+    y=f(x)
+    s+=y
+
+integral=5*s/N
+print(integral)
+
+
+
+def f(x):
+    return x**2
+
+xi=0
+xf=5
+n=100001
+h=(xf-xi)/n
+xx=[]
+yy=[]
+for i in range(n):
+    x=xi+i*h
+    xx.append(x)
+    yy.append(f(x))
+c=yy[0]+yy[n-1]
+s=0
+for i in range(1,n-1):
+    s+=2*yy[i]
+c+=s
+integral=h*c/2
+print(integral)
+
+
+def f(x):
+    return x**2
+xi=0
+xf=5
+n=100001
+h=(xf-xi)/n
+xx=[]
+yy=[]
+for i in range(n):
+    x=xi+i*h
+    xx.append(x)
+    yy.append(f(x))
+c=yy[0]+yy[n-1]
+s=0
+for i in range(1,n-1,2):
+    s+=4*yy[i]
+s1=0
+for i in range(2,n-1,2):
+    s1+=2*yy[i]
+c+=s+s1
+integral=h*c/3
+print(integral)
+
