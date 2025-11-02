64e3db62f849697c3bbfe19d62e4d2c8f8e07db9
Purusottam Rooj <purusottamrooj2021@gmail.com>
2025-11-02 20:54:14 +0530

    trial

--- Trial.py
+++ Trial.py
@@ -80,7 +80,7 @@ for i in range(2,n-1,2):
     s1+=2*yy[i]
 c+=s+s1
 integral=h*c/3
-print(integral)'''
+print(integral)
 
 import random
 N=2*365*24*60*60
@@ -96,4 +96,49 @@ for i in range(N):
 if c<c1:
     print("You'll get her ")
 else:
-    print("You'll never get her ")
+    print("You'll never get her ")'''
+
+import numpy as np
+
+n=4
+
+a=np.zeros((n,n),dtype=float)
+x=np.zeros(n,dtype=float)
+b=np.zeros(n,dtype=float)
+
+file=open("input.dat","r")
+
+i=o
+for linei in file:
+    temp=linei.split()
+    for j in range(n):
+        a[i][j]=temp[j]
+    b[i]=temp[n]
+    i+=1
+
+file.close()
+
+for k in range(n):
+    if a[k][k]==0:
+        for j in range(n):
+            ta=a[k][j]
+            a[k][j]=a[k+1][j]
+            a[k+1][j]=ta
+        tb=b[k]
+        b[k]=b[k+1]
+        b[k+1]=tb
+    for i in range(k+1,n):
+        m=a[i][k]/a[k][k]
+        b[i]=b[i]-m*b[k]
+        for j in range(k,n):
+            a[i][j]=a[i][j]+m*a[k][j]
+    
+x[n-1]=b[n-1]/a[n-1][n-1]
+for i in range(n-2,-1,-1):
+    sum=0
+    for j in range(i+1,n):
+        sum=sum+a[i][j]*x[j]
+    x[i]=(b[i]-sum)/a[i][i]
+
+for i in range(n):
+    print(x[i])
